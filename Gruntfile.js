a0709215c3b17724f0397d8614d6c0951089acc3
figraham <fi.sage.graham@gmail.com>
2019-08-08 20:00:59 -0600

    basic build lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,23 @@ module.exports = grunt => {
 
   const gruntConfig = {
 
+    eslint: {
+      source: {
+        options: {
+          configFile: 'lint/eslint-build.js',
+        },
+        src: ['src/**/*.js'],
+      },
+    },
+
   };
 
   grunt.initConfig(gruntConfig);
+
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('lint', [
+    'eslint:build',
+  ]);
 
 }
---
+++ lint/eslint-build.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": [
+    "./eslint.js",
+  ],
+  "rules": {
+    "no-undef": "off", // throws errors on p5 variables, see:https://eslint.org/docs/rules/no-undef
+  }
+};
